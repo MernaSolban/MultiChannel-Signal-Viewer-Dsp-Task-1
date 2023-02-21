79485b9935f85aee061b3a9298e61bd4aa833053
SharifEhab <Sharif.Mohammad02@eng-st.cu.edu.eg>
2023-02-22 01:03:21 +0200

    Minor changes in page layout
    Co-authored-by: omar-adel1 <omar-adel1@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-import streamlit as st 
-import pandas as pd
-import numpy as np
+import streamlit as st   #for web development
+import pandas as pd  #read csv( CSV (comma-separated values) file is a text file that has a specific format which allows data to be saved in a table structured format) , data manipulation
+import numpy as np  #numerical computations
+import time #simulating real time data,loops
+import plotly.express as px  #interactive charts
 
 ## Page Layout
-st.set_page_config(layout="wide",page_title="Signal Manipulator" , page_icon=":radio:")
+st.set_page_config(layout="wide",page_title="Signal viewer and manipulator" , page_icon=":radio:")  #title imp for Search engine optimization
 
 
 
 #############
 #Title  
 st.title("Signal Viewer Task 1 Demo") 
+
+#  Choose which signal to manipulate or control
+control_mode = st.selectbox(
+'Current signal',
+('Signal 1', 'Signal 2', 'Link'))
+
+placeholder=st.empty()
 
 #viewports
 with st.container():
@@ -18,7 +27,7 @@ with st.container():
 
 with viewport_1:
 #insert viewport1
- st.subheader("Viewport1")
+ st.subheader("First Signal")
  chart_data=pd.DataFrame(
 
     columns=['a', 'b', 'c'])
@@ -27,20 +36,21 @@ with viewport_1:
     
 with viewport_2:
 #insert viewport2
- st.subheader("Viewport2")
+ st.subheader("Second Signal")
 
 
 #controls
 with st.container():
     controls_col_1, controls_col_2 = st.columns(2)
 #upload signal
-add_signal_button1 = st.button("Add First Signal" , key="add_button1")
-add_signal_button2= st.button("Add second signal" , key="add_button2")
+with controls_col_1:
+   add_signal_button1 = st.button("Add First Signal" , key="add_button1")
+   
+with controls_col_2:  
+   add_signal_button2= st.button("Add second signal" , key="add_button2")
 
-#radio buttons
-control_mode = st.selectbox(
-'Current signal',
-('Viewport 1', 'Viewport 2', 'Both'))
+
+
 #zoom
 
 #pause
